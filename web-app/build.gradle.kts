81e679a89734d381420681faaadc3cfd302a10b9
Praneethpj <ifeathern@gmail.com>
2025-07-20 19:47:35 -0700

    Fixed conflict issue

--- web-app/build.gradle.kts
+++ web-app/build.gradle.kts
@@ -10,7 +10,6 @@ dependencies {
     implementation(project(":company"))
     implementation(project(":ledger"))
     implementation(project(":contact"))
-    implementation(project(":stock-module"))
 
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
@@ -21,8 +20,7 @@ dependencies {
     implementation("nz.net.ultraq.thymeleaf:thymeleaf-layout-dialect")
     implementation("io.github.wimdeblauwe:htmx-spring-boot:4.0.1")
     implementation("io.github.wimdeblauwe:htmx-spring-boot-thymeleaf:4.0.1")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.17.1")
-    //developmentOnly("org.springframework.boot:spring-boot-devtools")
+//    developmentOnly("org.springframework.boot:spring-boot-devtools")
 
     // converting images to pdf, invoices etc.
     implementation("com.itextpdf:itext7-core:9.2.0")
